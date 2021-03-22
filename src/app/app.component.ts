916efa498a0db70ff5038bb4670f35846906c11d
jienba <adamjienba@outlook.fr>
2021-03-22 01:52:38 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  isAuth = false;
+  appareilOne = 'Télévision';
+  appareilTwo = 'Machine  à laver';
+  appareilThree = 'Ordinateur';
+  constructor() {
+    setTimeout(
+      () =>{
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout!');
+
+  }
+}
